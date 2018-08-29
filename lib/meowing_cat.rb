50523ddc8cc36bfdc318f9c1c18776e1693700d1
Gavin Soko <sokolofg@gmail.com>
2018-08-29 16:19:17 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,10 +1,17 @@
 class Cat 
   
+  def initialize
+    @name = "Starfruit Master"
+  end
+  
   def meow
     puts "meow!"
   end
   
+  attr_accessor :name
   attr_accessor :meow
+  
 end
+
   
   ## code your solution here. 
